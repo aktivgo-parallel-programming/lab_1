dd8717479c887e438ab869168a3430d13eee31b5
aktivgo <AktivGo01062001@gmail.com>
2022-02-10 22:04:23 +0300

    upload

---
+++ task2/task2.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <ctime>
+
+int* create_matrix(int);
+void fill_matrix(int*, int, int, int);
+void delete_matrix(int*, int);
+void print_matrix(int**, int);
+int* multiply_matrix(int*, int*, int);
+double calculate_multiply_time(int*, int*, int);
+
+const int SIZE = 2048;
+const int LEFT_BORDER = -10;
+const int RIGHT_BORDER = 10;
+
+int main()
+{
+    srand(time(0));
+
+    int* matrix_1 = create_matrix(SIZE);
+    int* matrix_2 = create_matrix(SIZE);
+    fill_matrix(matrix_1, SIZE, LEFT_BORDER, RIGHT_BORDER);
+    fill_matrix(matrix_2, SIZE, LEFT_BORDER, RIGHT_BORDER);
+
+    /*print_matrix(matrix_1, SIZE);
+    std::cout << std::endl;
+    print_matrix(matrix_2, SIZE);
+    std::cout << std::endl;*/
+
+    double time = calculate_multiply_time(matrix_1, matrix_2, SIZE);
+    std::cout << "Time to multiply: " << time << "s" << std::endl;
+
+    delete_matrix(matrix_1, SIZE);
+    delete_matrix(matrix_2, SIZE);
+}
+
+int* create_matrix(int size) {
+    int* matrix = new int [size * size];
+
+    for (int i = 0; i < size * size; i++) {
+        matrix[i] = 0;
+    }
+
+    return matrix;
+}
+
+void fill_matrix(int* matrix, int size, int left_border, int right_border) {
+    for (int i = 0; i < size * size; i++) {
+        matrix[i] = rand() % left_border - right_border;
+    }
+}
+
+void delete_matrix(int* matrix, int size) {
+    delete[] matrix;
+}
+
+void print_matrix(int* matrix, int size) {
+    for (int i = 0; i < size * size; i++) {
+        if (i != 0 && i % size == 0) {
+            std::cout << std::endl;
+        }
+        std::cout << matrix[i] << " ";
+    }
+}
+
+int* multiply_matrix(int* matrix_1, int* matrix_2, int size) {
+    int* result = create_matrix(size);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            for (int k = 0; k < size; k++) {
+                result[size * i + j] += matrix_1[size * i + k] * matrix_2[size * k + j];
+            }
+        }
+    }
+
+    return result;
+}
+
+double calculate_multiply_time(int* matrix_1, int* matrix_2, int size) {
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    start = std::chrono::high_resolution_clock::now();
+    int* result = multiply_matrix(matrix_1, matrix_2, SIZE);
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    return diff.count();
+}
